ee523b9f809239a600203cf89685caaa7a45343f
evamike <mike.eva.89@gmail.com>
2022-12-04 22:41:14 +0100

    Cleaned up Title.js.

--- src/Title.js
+++ src/Title.js
@@ -1,5 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
 import React, { Component } from 'react';
 
 class Title extends Component {
@@ -9,9 +7,7 @@ class Title extends Component {
 
   componentDidMount() {
     const {REACT_APP_BACKEND_URL} = process.env
-    console.log('Backend url=', REACT_APP_BACKEND_URL)
     const apiUrl = `${REACT_APP_BACKEND_URL}/demo`;
-    // const apiUrl = '/demo';
     fetch(apiUrl)
     .then(response => response.json())
     .then((data) => this.setState({text: data.title}));
